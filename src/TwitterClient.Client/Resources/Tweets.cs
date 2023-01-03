ed2ff0b92aad2ca1109ed5a90ece7aab8a69b625
icalderazzo <ignaciocalderazzo@gmail.com>
2023-01-03 10:26:50 -0300

    Tweets resources

---
+++ src/TwitterClient.Client/Resources/Tweets.cs
@@ -0,0 +1,9 @@
+namespace TwitterClient.Client.Resources;
+
+internal static partial class Resources
+{
+    public const string LikeTweet = "users/{0}/likes";
+    public const string RetweetTweet = "users/{0}/retweets";
+    public const string CreateTweet = "tweets";
+    public const string DeleteTweet = "tweets/{0}";
+}
